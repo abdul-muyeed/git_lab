d4c43c02a3821d6f151ae143cbd690293a7e1b5e
Abdul Muyeed <abdulmuyeed47@gmail.com>
2023-08-23 11:34:16 +0600

    improvements made to the code

--- StudentList.java
+++ StudentList.java
@@ -3,28 +3,45 @@ import java.text.*;
 import java.util.*;
 
 public class StudentList {
+    public static String Reader(){
+            String dataLine ="";
+            try{
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(
+                                new FileInputStream(Constant.FILE_NAME)));
+                dataLine = reader.readLine();
+                reader.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+                return dataLine;
+        }
+    public static void Writer(String newStudent, String formattedDate){
+            try{
+                 BufferedWriter writer = new BufferedWriter(
+                        new FileWriter(Constant.FILE_NAME, true));
+                writer.write(", " + newStudent + "\nList last updated on " + formattedDate);
+                writer.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     public static void main(String[] args) {
         // Check if an argument is provided
         if (args.length == 0) {
             System.out.println(Constant.INVALID_MESSAGE);
             return;
         }
-
         String operation = args[0];
 
         if (operation.equals("a")) {
             // Load and display all student data
             System.out.println(Constant.LOADING);
             try {
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.FILE_NAME)));
-                String dataLine = reader.readLine();
-                String students[] = dataLine.split(",");
+                String students[] = Reader().split(",");
                 for (String student : students) {
                     System.out.println(student);
                 }
-                reader.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -33,17 +50,11 @@ public class StudentList {
             // Load data and display a random student
             System.out.println(Constant.LOADING);
             try {
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.FILE_NAME)));
-                String dataLine = reader.readLine();
-                String students[] = dataLine.split(",");
+                String students[] = Reader().split(",");
                 int bound = students.length;
                 Random random = new Random();
                 int randomIndex = random.nextInt(bound);
                 System.out.println(students[randomIndex]);
-
-                reader.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -52,15 +63,12 @@ public class StudentList {
             // Add a new student to the list
             System.out.println(Constant.LOADING);
             try {
-                BufferedWriter writer = new BufferedWriter(
-                        new FileWriter(Constant.FILE_NAME, true));
                 String newStudent = operation.substring(1);
                 Date currentDate = new Date();
                 String dateFormatPattern = "dd/MM/yyyy - hh:mm:ss a";
                 DateFormat dateFormat = new SimpleDateFormat(dateFormatPattern);
                 String formattedDate = dateFormat.format(currentDate);
-                writer.write(", " + newStudent + "\nList last updated on " + formattedDate);
-                writer.close();
+                Writer(newStudent, formattedDate);
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -69,18 +77,14 @@ public class StudentList {
             // Search for a specific student
             System.out.println(Constant.LOADING);
             try {
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.FILE_NAME)));
-                String dataLine = reader.readLine();
+                
                 String searchQuery = operation.substring(1);
-                boolean index = dataLine.contains(searchQuery);
+                boolean index = Reader().contains(searchQuery);
                 if (index) {
                     System.out.println("Student found!");
                 } else {
                     System.out.println("Student not found!");
                 }
-                reader.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -89,13 +93,8 @@ public class StudentList {
             // Count the number of words in the data
             System.out.println(Constant.LOADING);
             try {
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(
-                                new FileInputStream(Constant.FILE_NAME)));
-                String dataLine = reader.readLine();
-                String students[] = dataLine.split(",");
+                String students[] = Reader().split(",");
                 System.out.println(students.length + " word(s) found ");
-                reader.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
